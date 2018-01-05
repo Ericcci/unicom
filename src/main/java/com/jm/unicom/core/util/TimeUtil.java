0822e7f06aaf48e8fa502f59f29cff5ef1df4f0d
Ericcci <380652070@qq.com>
2018-01-05 16:44:16 +0800

    时间工具类

---
+++ src/main/java/com/jm/unicom/core/util/TimeUtil.java
@@ -0,0 +1,21 @@
+package com.jm.unicom.core.util;
+
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Calendar;
+
+/**
+ * <b>Description:</b><br>
+ * 计算目前到今天结束还有多少秒
+ *
+ * @author Eric.
+ * @version 1.0
+ *          <b>ProjectName:</b> unicom
+ *          <br><b>PackageName:</b> com.jm.unicom.core.util
+ *          <br><b>Date:</b> 2018/1/5 16:37
+ */
+public class TimeUtil {
+    public Long getTime() {
+        return 86400 - DateUtils.getFragmentInSeconds(Calendar.getInstance(), Calendar.DATE);
+    }
+}
